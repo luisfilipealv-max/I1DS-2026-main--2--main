bdcbd1355e151db3af83b5c13878f1b7cea538fd
Luis Filipe Alves <luisfilipealv@gmail.com>
2026-03-18 11:27:30 -0300

    utilizamos a funções para realizar exercicios de sistenmas de votos.

---
+++ I1DS-2026-main--2--main/I1DS-2026-main/atividades/03-exerciciofuncoes.js
@@ -0,0 +1,13 @@
+console.log("-----------------------------------------------------------------------------")
+function subtrair (valor1, valor2){
+    return valor1 - valor2
+}
+ console.log("2026 - 2010 =", subtrair (2026, 2010));
+
+ function multiplicacao (v1, v2){
+    return v1 * v2
+
+ }
+console.log("-----------------------------------------------------------------------------")
+ console.log("O carro tem a capacidade de 50 Litros,", "O carro pode fazer " , multiplicacao(60, 12), "Quilômetros" );
+console.log("-----------------------------------------------------------------------------")
