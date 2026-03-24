a621d276e9cf3043ee92829f0d0111fbee6ab9a2
Luis Filipe Alves <luisfilipealv@gmail.com>
2026-03-24 10:12:00 -0300

    conhecemos e aprendemos a utilizar imprimentos e deprementos, por hora é isso.

---
+++ I1DS-2026-main--2--main/I1DS-2026-main/aulas/12-lacoRepeticao.js
@@ -0,0 +1,7 @@
+// laço de repetição - FOR (PARA)
+// O professir te colocou de castigo para você escrever mil vezes a frase
+// "Eu vou prestar atenção as aulas e anotar tudo"
+
+for (let i = 0; i < 1000; i++) { // i-- == i = i = 1+1
+    
+}
